be773452466be5d63a6900ffa1ecb9b2e37da02b
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-10-01 20:49:52 -0500

    fix supervisor logic

--- lib/chat_supervisor.ex
+++ lib/chat_supervisor.ex
@@ -1,21 +1,30 @@
 defmodule Chat.Supervisor do
-    use Supervisor
-    
+    require Logger
+    use DynamicSupervisor
+
+    @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid()}
     def start_link(_arg) do
         DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
     end
 
 
+    @spec start_player(any) :: :error | {:ok, pid()}
     def start_player(id) do
         child_specification = %{
             id: id,
-            start: {Chat.Player, :start_link, [[]]}
+            start: {Chat.Player, :start_link, [id]}
         }
-        {:ok, pid} = DynamicSupervisor.start_link(__MODULE__, child_specification)
-        Registry.register(:player_lookup_table, id, pid)
-        {:ok, pid}
+        {:ok, pid} = DynamicSupervisor.start_child(__MODULE__, child_specification)
+        case Registry.register(:player_lookup_table, id, pid) do
+            {:ok, _} ->
+                Logger.info("NEW_PLAYER SUCCESS #{inspect pid}")
+                {:ok, pid}
+            {:error, _} ->
+                Logger.info("NEW_PLAYER FAILED")
+                :error
+        end
     end
-    
+
     @impl true
     def init(_arg) do
         DynamicSupervisor.init(strategy: :one_for_one)
